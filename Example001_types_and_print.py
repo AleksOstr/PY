bad7060ba57933732c074579cd4f71d470c30975
AleksOstr <ostryankoam@yandex.ru>
2022-09-22 15:06:32 +0400

    Example001 types, print, input etc

---
+++ Example001_types_and_print.py
@@ -0,0 +1,36 @@
+# типы данных и переменная
+# int, float, boolean, str, list, None
+
+# value = None
+# a = 123
+# b = 1.23
+# # print(type(a))
+# # print(type(b))
+# value = 1234
+# # print(type(value))
+# s = 'hello world'
+# print(a, '-', b, '-', s)
+# print('{1} - {2} - {0}'.format(a, b, s))
+# print(f'{a} - {b} - {s}')
+
+# f = True
+# print(f)
+
+# list = [1,2,3,4,5]
+# strings = []
+# print(list)
+
+# ввод и вывод информации
+# print('введите а')
+# a = int(input())
+# print('введите b')
+# b = int(input())
+# print(f'{a} + {b} = {a+b}')
+
+# Арифметические операции
+
+# a = 21
+# a //= 7
+# b = 4
+# c = a * b
+# print(c)
