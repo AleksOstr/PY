9f497aac2cc5081e9ca70564689a55d376aabab3
AlexOstryanko <ostryankoam@yandex.ru>
2022-10-10 17:39:25 +0400

    Homework05 Task4 part 1 RLE crypt final version

--- homework05/task4_1.py
+++ homework05/task4_1.py
@@ -1,9 +1,11 @@
 
 
-def rle_crypt(arr):
+def rle_crypt(file_name: str):
+    with open(file_name, 'r') as file:
+        data = file.readlines()
     count = 1
     crypted = []
-    for i in arr:
+    for i in data:
         for j in range(len(i)-1):
             if i[j] == i[j+1]:
                 count += 1
@@ -11,13 +13,12 @@ def rle_crypt(arr):
                 crypted.append([count, i[j]])
                 count = 1
         crypted.append([count, i[j+1]])
-    return crypted
+    with open('crypted_data.txt', 'w') as file:
+        file.write(' '.join(list(map(str, crypted))))
 
-with open('data.txt', 'r') as file:
-    data = file.readlines()
+rle_crypt('data.txt')
 
-with open('crypted_data.txt', 'w') as file:
-    file.write(' '.join(list(map(str, rle_crypt(data)))))
+
 
 
 
