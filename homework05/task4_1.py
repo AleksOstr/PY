29598e360ed684eb3626142bf57d5e4e1243ec62
AlexOstryanko <ostryankoam@yandex.ru>
2022-10-10 17:31:21 +0400

    Homework05 Task4 part1 RLE encrypt

--- homework05/task4_1.py
+++ homework05/task4_1.py
@@ -21,8 +21,3 @@ with open('crypted_data.txt', 'w') as file:
 
 
 
-# encrypted = ''
-# for i in crypted:
-#     for j in range(len(i)-1):
-#         encrypted += i[j] * i[j+1]
-# print(encrypted)
---
+++ homework05/task4_2.py
@@ -0,0 +1,7 @@
+def rle_decrypt()
+
+# encrypted = ''
+# for i in crypted:
+#     for j in range(len(i)-1):
+#         encrypted += i[j] * i[j+1]
+# print(encrypted)
