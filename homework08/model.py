76cc59450a6ac7a742e9bcf55914b7ee5510d89c
AlexOstryanko <ostryankoam@yandex.ru>
2022-10-22 16:13:31 +0400

    read csv done

--- homework08/model.py
+++ homework08/model.py
@@ -3,16 +3,16 @@ import csv
 import json
 
 def read_csv():
-    with open('databse.csv', 'r', encoding='utf-8') as file:
-        reader = csv.reader(file)
     database = []
-    for row in reader:
-        employee = {}
-        employee['id'] = row[0]
-        employee['last_name'] = row[1]
-        employee['first_name'] = row[2]
-        employee['position'] = row[3]
-        employee['salary'] = row[4]
-        employee['phone'] = row[5]
-        database.append(employee)
+    with open('database.csv', 'r', encoding='utf-8-sig') as file:
+        reader = csv.reader(file, delimiter=';')
+        for row in reader:
+            employee = {}
+            employee['id'] = int(row[0])
+            employee['last_name'] = row[1]
+            employee['first_name'] = row[2]
+            employee['position'] = row[3]
+            employee['salary'] = float(row[4])
+            employee['phone'] = row[5]
+            database.append(employee)
     return database
